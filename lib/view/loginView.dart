00fb061dea718857d58b3a7dcade971fc8243821
WeslleyMB <2003weslleymb@gmail.com>
2026-08-18 14:45:44 -0300

    feat: Modelo de Login - LoginModelView

--- lib/view/loginView.dart
+++ lib/view/loginView.dart
@@ -6,7 +6,7 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(title: const Text('Tela de Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
